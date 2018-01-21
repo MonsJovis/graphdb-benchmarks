8ef85b24736bfc11163f84b9ea9e7f6ca637726c
Markus Aurich <markus.aurich@gmail.com>
2018-01-21 21:07:28 +0100

    add log string to test logging

--- src/test/java/eu/socialsensor/main/GraphDatabaseBenchmarkTest.java
+++ src/test/java/eu/socialsensor/main/GraphDatabaseBenchmarkTest.java
@@ -1,16 +1,21 @@
 package eu.socialsensor.main;
 
 import static org.junit.Assert.fail;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
 public class GraphDatabaseBenchmarkTest
 {
+    Log log = LogFactory.getLog(GraphDatabaseBenchmarkTest.class);
     @Test
     public void testGraphDatabaseBenchmark()
     {
+
         GraphDatabaseBenchmark bench = new GraphDatabaseBenchmark(null /* inputPath */);
-        try
-        {
+        try{
+            log.debug("debug level enabled.");
             bench.run();
         }
         catch (Exception e)
