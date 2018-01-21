d2290c03c9021fb8ff9ac8855c663512218dac90
Markus Aurich <markus.aurich@gmail.com>
2018-01-21 21:10:40 +0100

    change logger

--- src/test/java/eu/socialsensor/main/GraphDatabaseBenchmarkTest.java
+++ src/test/java/eu/socialsensor/main/GraphDatabaseBenchmarkTest.java
@@ -4,18 +4,21 @@ import static org.junit.Assert.fail;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
 public class GraphDatabaseBenchmarkTest
 {
-    Log log = LogFactory.getLog(GraphDatabaseBenchmarkTest.class);
+    private static final Logger logger = LogManager.getLogger();
+
     @Test
     public void testGraphDatabaseBenchmark()
     {
 
         GraphDatabaseBenchmark bench = new GraphDatabaseBenchmark(null /* inputPath */);
         try{
-            log.debug("debug level enabled.");
+            logger.debug("debug level enabled.");
             bench.run();
         }
         catch (Exception e)
